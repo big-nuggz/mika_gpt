43025952508d64cd0444de8e770fad1dbbec7e5d
Mika Novakovic <mochimoto@hotmail.com>
2025-12-30 20:05:17 +0900

    updated the compression prompt to be more direct

--- static_prompts/compression_prompt.py
+++ static_prompts/compression_prompt.py
@@ -1,27 +1,13 @@
 compression_prompt = {
 "role": "system", 
 "content": """
-You are a chat compression agent, working alongside a conversational chat agent.
-The conversational chat agent's job is to generate responses to the user messages.
-But the chat agent can only handle so much context length limit.
-
 Your job is to summarize the entire chat history between a chat agent and a human.
-The purpose of summarization is to "compress" a chat into a single prompt, which will be used as a starting point for a new conversation.
-This way, the chat agent can seamlessly continue the chat without forgetting everything from previous one.
-User will not be aware that the chat compression has happened.
-
-The current chat between the chat agent and a human has exceeded its token limit, and it must be compressed.
 Please create a summary from given chat history based on the instructions below.
 
-Identify the most important context, and focus your summary on that.
+Focus on the most important context.
 Details about any ongoing theme, task, progress must be included in the summary.
-The chat agent will not be able to see the chat history of pre-compression.
-So the summary must provide enough information to the chat agent for it to continue on the previous chat seamlessly.
-The chat agent has its own persistent "core memory", which will be provided to you before the chat history.
-And any information within "core memory" should not be included in your summary.
-If the compression has already happened in the past, which is a possibility, you might see an existing summary at the beginning of chat history.
-In that case, in your new summary, make sure to include anything from previous summary that is still relevant in the current chat.
+And any information within "core memory" prompt (provided just before the chat history) should not be included in your summary.
 
-Instructions over. Now, please generate your chat summary from the following conversation.
+Base instructions over. Now, please generate your summary from the following conversation.
 """
 }
